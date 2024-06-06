516d2677ffa8395a665af2b7db8a7702ddccc128
jhonatanps <padua.jhonatan.soares@gmail.com>
2024-06-05 21:56:44 -0300

    aula-05-06

--- index.js
+++ index.js
@@ -6,9 +6,39 @@ const app = express();
 app.use(express.json());
 
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'produtos_mongo'
+});
 
+connection.connect((err) => {
+    if(err){
+        console.log("Erro ao conectar ao banco de dados", err)
+        return;
+    }
+    console.log("Conexão bem-sucedida")
+});
 
-
+app.get('/allprodutos', async (req, res) => {
+    try {
+        const results = await new Promise((resolve, reject) => {
+            connection.query('SELECT * FROM produtos', (err, results) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            })
+        })
+        console.log(results);
+        res.json(results);
+    } catch (error) {
+        console.log('Erro ao executar um consulta: ', error);
+        res.status(500).json({error: 'Erro ao executar um consulta'});
+    }
+})
 
 
 app.listen(3000, () => {
